1d81ff5b599061a4eb90a681bff034bfe716ea00
unknown <prabeshpathak50@gmail.com>
2022-05-16 20:57:01 +0545

    Adding router and controller

---
+++ routes/auth.js
@@ -0,0 +1,30 @@
+const {
+  registerUser,
+  generateAccessToken,
+  login,
+} = require("../controllers/auth");
+
+router.post("/register", async (req, res) => {
+  try {
+    const user = await registerUser(req.body);
+    const token = generateAccessToken(user);
+    res.status(201).send({ token, user });
+  } catch (error) {
+    if (error.code === 11000)
+      return res.status(409).send("Email already exists");
+    res.send(error.message);
+  }
+});
+
+// Register Login user
+router.post("/login", async (req, res) => {
+  try {
+    const token = await login(req.body);
+    if (token) return res.send(token);
+    res.status(401).send("Email or password doesn't match");
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+
+module.exports = router;
