89114da57c8fcae786380dc3bf4c3ad99216b0d9
shirsak123 <shirsak.shrestha@gmail.com>
2022-05-17 16:38:27 +0545

    created action for login logout reducer

---
+++ client/src/state/actions.js
@@ -0,0 +1,46 @@
+import api, { setAuthToken } from "../axios"
+import { SIGNIN_SUCCESS, LOGOUT } from "./types"
+
+export const login = (user) => async (dispatch) => {
+    dispatch({
+        type: SIGNIN_SUCCESS,
+        payload: user
+    })
+
+}
+
+export const logout = () => async (dispatch) => {
+    localStorage.removeItem("token")
+    setAuthToken(null)
+    dispatch({
+        type: LOGOUT,
+    })
+}
+
+// Retrieve and auth user if token exists in local storage
+export const retrieveSession = () => async dispatch => {
+    if (localStorage.getItem("token")) {
+        const token = localStorage.getItem("token")
+        setAuthToken(token)
+        try {
+            const user = await api.get("/signin/user")
+            if (user)
+                dispatch({
+                    type: SIGNIN_SUCCESS,
+                    payload: user.data
+                })
+        } catch (error) {
+            localStorage.removeItem("token")
+            setAuthToken(null)
+            console.log(error);
+        }
+    } else {
+        setAuthToken(null)
+        dispatch({
+            type: LOGOUT,
+        })
+    }
+}
+
+
+
