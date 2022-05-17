0fb1e81ae5425860f718aec2295dcebb637e3d7d
AnuraghSarkar <anuraghtimalsina@gmail.com>
2022-05-17 18:29:22 +0545

    :fire: changed hashroute to browserrouter

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter, Switch, Route } from "react-router-dom";
+import { useLocation, Switch, Route } from "react-router-dom";
 import Nav from "./components/Nav";
 import SignIn from './components/SignIn'
 import { useEffect, useState } from "react";
@@ -9,18 +9,17 @@ function App({ retrieveSession }) {
 	const [formOpen, setFormOpen] = useState(false)
 	const [navModals, setNavModals] = useState({ projects : false, teams : false })
 	useEffect(retrieveSession, [retrieveSession])
+	  const location = useLocation();
 
 	return (
-		<HashRouter>
 			<div className="App">
 				<Nav setFormOpen={setFormOpen} navModals={navModals} setNavModals={setNavModals} />
 				<div className="nav-padding" onClick={() => setNavModals({ projects: false, teams: false })}>
-					<Switch>
+					<Switch location={location} key={location.pathname}>
 						<Route exact path="/signin" component={SignIn} />
 					</Switch>
 				</div>
 			</div>
-		</HashRouter>
 	);
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from "react-redux"
+import { BrowserRouter } from "react-router-dom";
 import store from './state/store';
 import './index.css';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
