86f7487d5b9a92089474d48457d227cfcfd2a047
unknown <prabeshpathak50@gmail.com>
2022-05-17 19:31:08 +0545

    config(Server): Start server configuration

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
+const routes = require("./routes");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+require("dotenv").config();
+if (process.env.NODE_ENV == "development") {
+  mongoose
+    .connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connection with DB established");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+// Routes
+app.use(routes);
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => console.log("listening on " + port));
