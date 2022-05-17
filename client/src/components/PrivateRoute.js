272dcc86ba9194856e43ab3e8d157a130e22ebb3
anishhimself <57608042+anishhimself@users.noreply.github.com>
2022-05-17 16:53:29 +0545

    Created PrivateRoutes for users

---
+++ client/src/components/PrivateRoute.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Redirect, Route } from "react-router-dom";
+import { connect } from "react-redux";
+
+const PrivateRoute = ({
+	isAuthenticated,
+	loading,
+	component: Component,
+	...rest
+}) => (
+	<Route
+		{...rest}
+		render={(props) => {
+			if (!loading)
+				return isAuthenticated ? (
+					<Component {...props} />
+				) : (
+					<Redirect to="/signin" />
+				);
+		}}
+	/>
+);
+
+const mapStateToProps = (state) => ({
+	isAuthenticated: state.reducer.isAuthenticated,
+	loading: state.reducer.loading,
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
