226ea0b85bf01d905148705a876a97b4ff70f2f2
AnuraghSarkar <anuraghtimalsina@gmail.com>
2022-05-18 15:43:22 +0545

    :fire: completed the authentication

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
 import { Redirect } from "react-router";
 import { login } from "../state/actions";
