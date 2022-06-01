1420a66fb6745f5b736518665486b11413e78bbe
AnuraghSarkar <anuraghtimalsina@gmail.com>
2022-06-01 18:06:58 +0545

    :tada: issue schema created prefix issue

--- models/issue.js
+++ models/issue.js
@@ -1,4 +1,41 @@
 const mongoose = require("mongoose");
 
-const issueSchema = mongoose.Schema({});
+const issueSchema = mongoose.Schema(
+  {
+    summary: {
+      type: String,
+      required: true,
+    },
+    issueType: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "ProjectSchema",
+      required: true,
+    },
+    assignee: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "UserSchema",
+    },
+    labels: [String],
+    issueStatus: {
+      type: String,
+      default: "to do",
+    },
+    sprint: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "SprintSchema",
+    },
+    epic: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "IssueSchema",
+    },
+  },
+  { timestamps: true }
+);
+
+
 module.exports = mongoose.model("IssueSchema", issueSchema);
