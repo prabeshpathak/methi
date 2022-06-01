543404fc56ea067b65b07144bb85a33fed1d37ce
gauravchaulagain <gauravchaulagain45@gmail.com>
2022-06-01 20:50:49 +0545

    created routes for issue

---
+++ api/issue.js
@@ -0,0 +1,100 @@
+const { authenticateToken, authorizations } = require('../controllers/auth');
+const { canComment, getComments } = require('../controllers/comment');
+const { createIssue, getIssues, updateIssue, backlogIssues, getIssue, epicIssues, deleteIssue, getRoadmap } = require('../controllers/issue');
+const { getSprints } = require('../controllers/sprint');
+const { getTeamAssignees } = require('../controllers/team');
+
+const router = require('express').Router()
+
+// Create new issue
+router.post("/create", authenticateToken, authorizations, async (req, res) => {
+    try {
+        const issue = await createIssue(req.body)
+        res.send(issue)
+    } catch (error) {
+        res.send(error.message);;
+    }
+})
+
+
+// Get all issues for backlog of a project
+router.get("/backlog/:id", authenticateToken, async (req, res) => {
+    try {
+        const issues = await backlogIssues(req.params.id)
+        res.send(issues)
+    } catch (error) {
+        res.send(error.message);;
+    }
+})
+
+// Get all epic of a project
+router.get("/roadmap/:id", authenticateToken, async (req, res) => {
+    try {
+        const issues = await getRoadmap(req.params.id)
+        res.send(issues)
+    } catch (error) {
+        res.send(error.message);;
+    }
+})
+
+// Get all detail of an issue
+router.get("/:id", authenticateToken, async (req, res) => {
+    try {
+        const issue = await getIssue(req.params.id)
+        if(!issue)
+        return res.status(404).send("Issue not found")
+        const epics = await epicIssues(issue.project)
+        const sprints = await getSprints(issue.project._id)
+        const assignees = await getTeamAssignees(issue.project.lead)
+        const comments = await getComments(req.params.id)
+        const commentPermission = await canComment(req.params.id, req.user._id) ? true : false
+        res.send({issue, epics, sprints, assignees, comments, commentPermission})
+    } catch (error) {
+        res.send(error.message);;
+    }
+})
+
+// Update issue
+router.patch("/:id", authenticateToken, authorizations, async (req, res) => {
+    try {
+        await updateIssue(req.params.id, req.body)
+        res.send("Issue updated")
+    } catch (error) {
+        res.send(error.message);;
+    }
+})
+
+// Update issue status
+router.patch("/status/:id", authenticateToken, async (req, res) => {
+    try {
+        await updateIssue(req.params.id, { issueStatus : req.body.issueStatus})
+        res.send("Issue updated")
+    } catch (error) {
+        res.send(error.message);;
+    }
+})
+
+// Delete issue
+router.delete("/:id", authenticateToken, async (req, res) => {
+    try {
+        if(await deleteIssue(req.params.id, req.user._id))
+            res.send("Issue deleted")
+        else 
+            res.status(401).send("Not authorized");
+    } catch (error) {
+        res.send(error.message);;
+    }
+})
+
+// Get all issues assigned to user
+router.get("/", authenticateToken, async (req, res) => {
+    try {
+        const issues = await getIssues(req.user._id)
+        res.send(issues)
+    } catch (error) {
+        res.send(error.message);;
+    }
+})
+
+
+module.exports = router
