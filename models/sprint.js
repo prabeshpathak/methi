e8de9f8a6ac50c4dfd71d1e1c5d747072b0ce1ce
AnuraghSarkar <anuraghtimalsina@gmail.com>
2022-06-01 18:10:51 +0545

    :tada: sprint  schema created prefix issue

--- models/sprint.js
+++ models/sprint.js
@@ -1,5 +1,24 @@
 const mongoose = require("mongoose");
 const Issue = require("./issue");
 
-const sprintSchema = mongoose.Schema({});
+const sprintSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  startDate: Date,
+  endDate: Date,
+  project: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "ProjectSchema",
+    required: true,
+  },
+  isActive: {
+    type: Boolean,
+    default: false,
+  },
+  goal: String,
+});
+
+
 module.exports = mongoose.model("SprintSchema", sprintSchema);
