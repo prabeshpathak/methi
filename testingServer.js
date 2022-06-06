d9b6c48c679fcdc0ee9311678735dca81a93b58e
notThePrabesh <prabeshpathak50@gmail.com>
2022-06-06 21:30:55 +0545

    TDD: User routes testing

---
+++ testingServer.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
+// const routes = require("./routes");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// Routes
+app.use("/api/signin", require("./api/signin"));
+
+module.exports = app;
