917aebcca9f698237007dc775256992e9d421796
AnuraghSarkar <anuraghtimalsina@gmail.com>
2022-06-07 12:25:06 +0545

    :green_heart: working on issue component

---
+++ client/src/components/Issue.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useRef, useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import api from "../axios";
+import IssueLoader from "./IssueLoader";
+import { setNotification } from "../state/actions";
+
+const Issue = ({}) => {};
+
+export default Issue;
