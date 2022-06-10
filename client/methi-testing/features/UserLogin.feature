09e2adfb2d2d4b1411555d1ccf71023a00fd4ff5
anishhimself <57608042+anishhimself@users.noreply.github.com>
2022-06-10 21:08:04 +0545

    view assigned projects in homepage

---
+++ client/methi-testing/features/UserLogin.feature
@@ -0,0 +1,5 @@
+Feature: To Login 
+  As a user, I need to login to the system after registration, so that I can access the system.
+
+  Scenario: User fills Email address and press continue button and again fill password and press signIn button to get access to the application
+    Given Test login functionality
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,12 +1,64 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import api from "../axios";
+import { ProjectsLoader } from "./ProjectsLoader";
+import { connect } from "react-redux";
 
-const Profile = () => {
+async function fetchData() {
+	const data = {};
+	let response = await api.get("/projects");
+	data.projects = response.data;
+	response = await api.get("/issues");
+	data.issues = response.data;
+	return data;
+}
 
-  return (
-    <div className="project-settings mt-5">
-      <h1>Working on create project</h1>
-    </div>
-  );
+const Home = () => {
+	const [loading, setLoading] = useState(true);
+	const [projects, setProjects] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			const data = await fetchData();
+			setProjects(data.projects);
+			setIssues(data.issues);
+			setLoading(false);
+		})();
+	}, []);
+
+
+
+	return (
+		<div className="home container">
+			<h2>Your work</h2>
+			<h4 className="home__label">Recent projects</h4>
+			<div className="home__projects">
+				{loading ? (
+					<ProjectsLoader type="projects" />
+				) : (
+					projects.map((p) => (
+						<Link
+							className="home__project"
+							key={p._id}
+							to={`/projects/boards/${p._id}`}
+						>
+							<h4>{p.title}</h4>
+							<p>Team managed software</p>
+						</Link>
+					))
+				)}
+			</div>
+			<h4>Assigned to me</h4>
+			<div className="home__assigned">
+			
+			</div>
+		</div>
+	);
 };
 
+const mapStateToProps = (state) => ({
+	created: state.reducer.created,
+	user: state.reducer.user,
+});
 
-export default Profile;
+export default connect(mapStateToProps, { issueCreatedDone, setNotification })(Home);
