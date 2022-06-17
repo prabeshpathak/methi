806739f0f9b40b350a7ffee8fa2117f9c493a95d
notThePrabesh <prabeshpathak50@gmail.com>
2022-06-17 19:30:52 +0545

    feat(roadmap): escape button for canceling provided effectt

--- client/src/components/Roadmap.js
+++ client/src/components/Roadmap.js
@@ -28,6 +28,8 @@ const Roadmap = ({ match, created, user, issueCreatedDone }) => {
     function handleEscape(e) {
       if (e.key === "Escape") setCreateIssue(false);
     }
+    window.addEventListener("keydown", handleEscape);
+    return () => window.removeEventListener("keydown", handleEscape);
   }, []);
 
   return (
