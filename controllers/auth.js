0eb2dd5f16bcc8a876892d85f6f1eb050cd48f66
AnuraghSarkar <anuraghtimalsina@gmail.com>
2022-06-21 15:51:18 +0545

    :recycle: optimized the auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,43 +1,82 @@
+// importing modules
 const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 const bcrypt = require("bcryptjs");
 const Project = require("../models/project");
+const express = require("express");
 
+// exporting the registered user model and getting the user
 exports.registerUser = async (data) => await User.create(data);
+exports.getUser = async (id) => await User.findById(id);
 
-exports.getUser = async id => await User.findById(id)
-
-exports.login = async ({ email, password }) => {
-	const user = await User.findOne({ email }).select("+password")
-	if(!user)
-		return null;
-	if(await bcrypt.compare(password, user.password)) {
-		const token = this.generateAccessToken(user)
-		const decoded = jwt.verify(token, process.env.TOKEN_SECRET)
-		return { token, user : decoded }
-	}
-	else
-		return null;
-}
-
-exports.generateAccessToken = (user) =>
-	jwt.sign(user.toJSON(), process.env.TOKEN_SECRET, { expiresIn: "6h" });
-
-exports.authenticateToken = (req, res, next) => {
-	const header = req.header("Authorization");
-	const token = header && header.split(" ")[1];
-	if (token == null) return res.sendStatus(401);
-
-	jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-		if (err) return res.status(403).send("Token is invalid");
-		req.user = user;
-		next();
-	});
+/**
+ *
+ * @param {number} uid - user id
+ * @param {string} data - data to be updated
+ * @returns {User} updated user
+ */
+exports.updateUser = async (uid, data) => {
+  const u = await User.findByIdAndUpdate(uid, data);
+  return u;
 };
 
+/**
+ *
+ * @param {string} email - user email
+ * @param {string} password - user password
+ * @returns {object} user - user object
+ */
+exports.login = async ({ email, password }) => {
+  // find user by email and select the respective password
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) return null;
+  // compare the password with the hashed password and return the user if the password is correct and decode the token
+  if (await bcrypt.compare(password, user.password)) {
+    const token = this.generateAccessToken(user);
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+    return { token, user: decoded };
+  } else return null;
+};
+
+/**
+ *
+ * @param {object} user - user object
+ * @returns {string} - jwt token for the user along with the expiry time
+ */
+exports.generateAccessToken = (user) =>
+  jwt.sign(user.toJSON(), process.env.TOKEN_SECRET, { expiresIn: "6h" });
+
+/**
+ * @param {express.Request} req - request object
+ * @param {express.Response} res - response object
+ * @param {express.NextFunction} next - next function
+ * @returns {object} - return the verified user object
+ */
+exports.authenticateToken = (req, res, next) => {
+  // get the token from the header and check if it is valid
+  const header = req.header("Authorization");
+  const token = header && header.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+  // verify the token and return the user object
+  jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+    if (err) return res.status(403).send("Token is invalid");
+    req.user = user;
+    next();
+  });
+};
+
+/**
+ * @param {express.Request} req - request object
+ * @param {express.Response} res - response object
+ * @param {express.NextFunction} next - next function
+ * @returns {express.Response} - return the response object where user can be authorized to access the project
+ */
+
 exports.authorizations = async (req, res, next) => {
-	const project = await Project.findById(req.body.project).select("lead")
-    if(!project || project.lead.toString() !== req.user._id)
-		return res.status(401).send("Not authorized to modify project");
-	next();
+  // find the project where the signed in user is a lead
+  const project = await Project.findById(req.body.project).select("lead");
+  // check if the user is a lead of the project
+  if (!project || project.lead.toString() !== req.user._id)
+    return res.status(401).send("Not authorized to modify project");
+  next();
 };
