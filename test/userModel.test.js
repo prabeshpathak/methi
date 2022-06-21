5802a24fa558840dfccbf132fc0997e8c82c0586
notThePrabesh <prabeshpathak50@gmail.com>
2022-06-21 20:19:52 +0545

    update user model test

--- test/userModel.test.js
+++ test/userModel.test.js
@@ -1,70 +1,58 @@
 const mongoose = require("mongoose");
 const userSchema = require("../models/user");
-const docc = {
-    email: "new@gmail.com",
-};
-
-
-
-afterAll(async () => {
-  await mongoose.connection.close();
-});
 
 // use the new name of the database
-const url =
-beforeAll(async () => {
-    require("dotenv").config();
-    await mongoose.connect(process.env.MONGODB_URI_TEST,{
+const url = beforeAll(async () => {
+  require("dotenv").config();
+  await mongoose.connect(process.env.MONGODB_URI_TEST, {
     useNewUrlParser: true,
     // useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // useFindAndModify: false,
-   
-
-    
   });
   await userSchema.deleteMany();
 });
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
+
 describe("User Test", () => {
   // the code below is for insert testing
   it("Adding new user", () => {
     const user = {
-      email: "new2@gmail.com",
+      email: "test@gmail.com",
       password: "test12345",
+      fullName: "test testing",
     };
 
     return userSchema.create(user).then((pro_ret) => {
-      expect(pro_ret.email).toEqual("new2@gmail.com");
+      expect(pro_ret.email).toEqual("test@gmail.com");
     });
   });
 
-
-// //   //get one user
+  // //   //get one user
   it("get/user--> get any one user", async () => {
-    const pr = await userSchema.findOne({email: "new2@gmail.com"});
+    const pr = await userSchema.findOne({ email: "test@gmail.com" });
     expect(1).toBe(1);
   });
 
+  it("to test the update", async () => {
+    return await userSchema
+      .findOneAndUpdate(
+        { email: "test@gmail.com" },
+        { $set: { fullName: "new test" } },
+        { new: true }
+      )
+      .then((val) => {
+        expect(val.fullName).toEqual("new test");
+      });
+  });
 
-//   update user
-//   it("to test the update", async () => {
-//     return userSchema.findOneAndUpdate(
-//       {email: "test@gmail.com" },
-//       { $set: { password: "test12345" } },
-//       { new: true }
-//     ).then((doccc) => {
-//       expect(doccc.password).toEqual("test12345");
-//     });
-//   });
-
-
-//     // the code below is for delete testing
-    // it("Deleting new added", async () => {
-    //   const status = await userSchema.deleteOne({ email: "testss@gmail.com" });
-    //   expect(status.ok).toBe(1);
-    // });
+  //     // the code below is for delete testing
+  // it("Deleting new added", async () => {
+  //   const status = await userSchema.deleteOne({ email: "testss@gmail.com" });
+  //   expect(status.ok).toBe(1);
+  // });
 });
