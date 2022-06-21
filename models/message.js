3c797fe92dd2f914b82913e921de60a7023dbbd5
AnuraghSarkar <anuraghtimalsina@gmail.com>
2022-06-21 15:37:11 +0545

    :recycle: optimized message model

--- models/message.js
+++ models/message.js
@@ -1,31 +1,34 @@
 const mongoose = require("mongoose");
-const Team = require("../models/team")
+const Team = require("../models/team");
 
-const messageSchema = mongoose.Schema({
+// Create a schema for the message model field type [String, Number, Date, Buffer, Boolean, ObjectId, Array]
+const messageSchema = mongoose.Schema(
+  {
     body: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    user : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "UserSchema",
-        required : true
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "UserSchema",
+      required: true,
     },
-    team : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "TeamSchema",
-        required : true
-    }
-}, { timestamps: true });
+    team: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "TeamSchema",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 messageSchema.pre("save", async function (next) {
-    if (this.isNew) {
-        const team = await Team.findById(this.team)
-        team.messages.push(this)
-        await team.save()
-    }
-    next()
-})
-
+  if (this.isNew) {
+    const team = await Team.findById(this.team);
+    team.messages.push(this);
+    await team.save();
+  }
+  next();
+});
 
 module.exports = mongoose.model("MessageSchema", messageSchema);
