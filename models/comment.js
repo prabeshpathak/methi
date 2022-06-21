9d5ed50ea3c91dbefa7dc048fa6721030934afc6
AnuraghSarkar <anuraghtimalsina@gmail.com>
2022-06-21 15:36:15 +0545

    :recycle: optimized comment model

--- models/comment.js
+++ models/comment.js
@@ -1,20 +1,24 @@
 const mongoose = require("mongoose");
 
-const commentSchema = mongoose.Schema({
+// Create a schema for the comment model field type [String, Number, Date, Buffer, Boolean, ObjectId, Array]
+const commentSchema = mongoose.Schema(
+  {
     body: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    user : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "UserSchema",
-        required : true
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "UserSchema",
+      required: true,
     },
-    issue : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "IssueSchema",
-        required : true
-    }
-}, { timestamps: true });
+    issue: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "IssueSchema",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("CommentSchema", commentSchema);
