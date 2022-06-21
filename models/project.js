894689c309ef650a19b7067743d6cc566c9a79e6
AnuraghSarkar <anuraghtimalsina@gmail.com>
2022-06-21 15:37:35 +0545

    :recycle: optimized project model

--- models/project.js
+++ models/project.js
@@ -1,25 +1,30 @@
 const mongoose = require("mongoose");
+const Issue = require("./issue");
 
+// Create a schema for the project model field type [String, Number, Date, Buffer, Boolean, ObjectId, Array]
 
-const projectSchema = mongoose.Schema({
+const projectSchema = mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    key : {
-        type : String,
-        required : true,
+    key: {
+      type: String,
+      required: true,
     },
-    lead : {
-        type : mongoose.Schema.Types.ObjectId,
-        ref : "UserSchema"
-    }
-}, { timestamps: true });
+    lead: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "UserSchema",
+    },
+  },
+  { timestamps: true }
+);
 
-projectSchema.pre("findOneAndDelete", async function(next) {
-    const pid = this.getFilter()["_id"]
-    await Issue.deleteMany({ project : pid })
-    next()
-})
+projectSchema.pre("findOneAndDelete", async function (next) {
+  const pid = this.getFilter()["_id"];
+  await Issue.deleteMany({ project: pid });
+  next();
+});
 
 module.exports = mongoose.model("ProjectSchema", projectSchema);
