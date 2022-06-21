13a8d6d96a03953f765ce2398450a109cc958b1d
notThePrabesh <prabeshpathak50@gmail.com>
2022-06-21 21:05:08 +0545

    team model testing for adding new team

--- test/Team/teamModel.test.js
+++ test/Team/teamModel.test.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const teamSchema = require("../../models/team");
 
 // use the new name of the database
-const url = beforeAll(async () => {
+beforeAll(async () => {
   require("dotenv").config();
   await mongoose.connect(process.env.MONGODB_URI_TEST, {
     useNewUrlParser: true,
@@ -11,11 +11,26 @@ const url = beforeAll(async () => {
     useUnifiedTopology: true,
     // useFindAndModify: false,
   });
-  await userSchema.deleteMany();
+  await teamSchema.deleteMany();
 });
 
 afterAll(async () => {
   await mongoose.connection.close();
 });
 
-describe("Testing container for Team", () => {});
+describe("Testing container for Team", () => {
+  // the code below is for insert testing
+  it("Adding New Team", () => {
+    const teams = new teamSchema();
+
+    teams.lead = "4edd40c86762e0fb12000001";
+    teams.members = ["4edd40c86762e0fb12000001"];
+    teams.title = "Team Test";
+    teams.description = "test description";
+    teams.messages = ["4edd40c86762e0fb12000001"];
+
+    return teams.save().then((pro_ret) => {
+      expect(pro_ret.title).toEqual("Team Test");
+    });
+  });
+});
