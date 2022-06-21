bcbfec7a5aec2f4d6a4e24fc53a762bb7722f710
AnuraghSarkar <anuraghtimalsina@gmail.com>
2022-06-21 15:39:20 +0545

    :recycle: optimized user model

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
+// Create a schema for the user model field type [String, Number, Date, Buffer, Boolean, ObjectId, Array]
 const userSchema = mongoose.Schema({
   email: {
     type: String,
@@ -15,6 +16,8 @@ const userSchema = mongoose.Schema({
     minlength: 6,
   },
 });
+
+// Hash the password before saving-to-db (pre-save hook) (https://mongoosejs.com/docs/middleware.html)
 
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) next();
