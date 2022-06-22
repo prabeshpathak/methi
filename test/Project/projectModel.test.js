4997314a11f567b3a24874cda2e5dbfa292314eb
notThePrabesh <prabeshpathak50@gmail.com>
2022-06-22 15:57:59 +0545

    complete model testing for project

--- test/Project/projectModel.test.js
+++ test/Project/projectModel.test.js
@@ -21,14 +21,33 @@ afterAll(async () => {
 describe("Testing container for Project", () => {
   // the code below is for insert testing
   it("Adding New Project", () => {
-    const teams = new projectSchema();
+    const project = new projectSchema();
 
-    teams.title = "Team Test";
-    teams.key = "4edd40c86762e0fb12000001";
-    teams.lead = ["4edd40c86762e0fb12000001"];
+    project.title = "Team Test";
+    project.key = "4edd40c86762e0fb12000001";
+    project.lead = ["4edd40c86762e0fb12000001"];
 
-    return teams.save().then((pro_ret) => {
+    return project.save().then((pro_ret) => {
       expect(pro_ret.title).toEqual("Team Test");
     });
+  });
+
+  // the code below is for get testing
+  it("Getting Project", async () => {
+    const project = await projectSchema.findOne({ title: "Team Test" });
+    expect(project.title).toEqual("Team Test");
+  });
+
+  // the code below is for updating project
+  it("Updating Project", async () => {
+    return await projectSchema
+      .findOneAndUpdate(
+        { title: "Team Test" },
+        { $set: { key: "new test" } },
+        { new: true }
+      )
+      .then((val) => {
+        expect(val.key).toEqual("new test");
+      });
   });
 });
