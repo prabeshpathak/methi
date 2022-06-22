76140e9d820834dde9fbd6e77cedaae099da2b50
notThePrabesh <prabeshpathak50@gmail.com>
2022-06-22 16:02:24 +0545

    adding for issue model

--- test/Issue/issueModal.test.js
+++ test/Issue/issueModal.test.js
@@ -18,4 +18,22 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-describe("Testing container for Issue", () => {});
+describe("Testing container for Issue", () => {
+  // the code below is for insert testing
+  it("Adding New Issue", () => {
+    const issue = new issueSchema();
+
+    issue.summary = "Team Test";
+    issue.issueType = "Team Test";
+    issue.description = "Team Test";
+    issue.project = "4edd40c86762e0fb12000001";
+    issue.assignee = "4edd40c86762e0fb12000001";
+    issue.labels = ["hi"];
+    issue.sprint = ["4edd40c86762e0fb12000001"];
+    issue.epic = ["4edd40c86762e0fb12000001"];
+
+    return issue.save().then((pro_ret) => {
+      expect(pro_ret.summary).toEqual("Team Test");
+    });
+  });
+});
