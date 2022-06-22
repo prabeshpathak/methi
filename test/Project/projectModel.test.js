18b0b4351e248a99da9379e7bb3a9e45ca6c5731
notThePrabesh <prabeshpathak50@gmail.com>
2022-06-22 15:55:56 +0545

    adding new project test model

--- test/Project/projectModel.test.js
+++ test/Project/projectModel.test.js
@@ -18,4 +18,17 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-describe("Testing container for Project", () => {});
+describe("Testing container for Project", () => {
+  // the code below is for insert testing
+  it("Adding New Project", () => {
+    const teams = new projectSchema();
+
+    teams.title = "Team Test";
+    teams.key = "4edd40c86762e0fb12000001";
+    teams.lead = ["4edd40c86762e0fb12000001"];
+
+    return teams.save().then((pro_ret) => {
+      expect(pro_ret.title).toEqual("Team Test");
+    });
+  });
+});
