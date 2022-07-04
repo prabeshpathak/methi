dbb1305ff2e9f0c9a185f285be68f8570c03404f
AnuraghSarkar <anuraghtimalsina@gmail.com>
2022-07-04 14:24:11 +0545

    :hammer: moved dotenv out of devdepn

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://methi.herokuapp.com/api",
 });
 
 export const setAuthToken = (token = localStorage.getItem("token")) => {
